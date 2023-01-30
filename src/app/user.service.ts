7b3eadc8f674ade6d438e12d90d17284f0facb54
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-01-30 19:02:32 +0100

    Subjects in Observables

---
+++ src/app/user.service.ts
@@ -0,0 +1,8 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs-compat";
+
+@Injectable({providedIn: 'root'})
+export class UserService {
+    //activatedEmitter = new EventEmitter<boolean>();
+    activatedEmitter = new Subject<boolean>();
+}
