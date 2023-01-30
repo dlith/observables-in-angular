2a6df28721cc86a571ea5965c734644ea80514fa
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-01-30 18:36:01 +0100

    Errors & Completion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,6 +22,12 @@ export class HomeComponent implements OnInit, OnDestroy {
         setInterval(
           ()=> {
             observer.next(count);
+            if(count == 5){
+              observer.complete();
+            }
+            if(count>3){
+              observer.error(new Error('Count is greater 3!'));
+            }
             count++;
           }, 1000
         )
@@ -30,6 +36,11 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     this.firstObsSubscription = customIntervalObservable.subscribe(data=> {
       console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
     });
   }
 
